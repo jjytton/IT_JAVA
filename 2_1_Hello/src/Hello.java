32cfbb837b81a9f5d8dd949d93ae9871b51eb430
JungYoungJeong <jjytton@naver.com>
2016-03-16 10:55:37 +0900

    2day

---
+++ 2_1_Hello/src/Hello.java
@@ -0,0 +1,11 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.println("java!!");
+		System.out.printf("%s %d \n","bb",100);
+	}
+
+}
